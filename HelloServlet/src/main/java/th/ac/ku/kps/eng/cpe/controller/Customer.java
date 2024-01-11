ba115a2c91a3405bb9b729aca4ca21538fcb4c6e
Ryker212 <103985410+Ryker212@users.noreply.github.com>
2024-01-11 21:41:26 +0700

    เพื่ม ListPhone
    เพื่ม ListPhone
    -addmethod

--- HelloServlet/src/main/java/th/ac/ku/kps/eng/cpe/controller/Customer.java
+++ HelloServlet/src/main/java/th/ac/ku/kps/eng/cpe/controller/Customer.java
@@ -1,10 +1,20 @@
 package th.ac.ku.kps.eng.cpe.controller;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Customer {
 	private String name;
-	
+
+	private List<PhoneNumber> phoneNumbers;
+
 	public Customer(String name) {
-		
 		this.name = name;
+	}
+
+	@JsonProperty("name")
+	public String getCustomer() {
+		return name;
 	}
 }

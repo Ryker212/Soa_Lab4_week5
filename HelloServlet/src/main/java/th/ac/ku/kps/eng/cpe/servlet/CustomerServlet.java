e43c7b1e18d84471138c8b52cd7ddbc97b9e641b
Ryker212 <103985410+Ryker212@users.noreply.github.com>
2024-01-12 13:06:20 +0700

    update CustomerServlet
    -เพิ่มmethod bodyมาอ่านค่า ที่client ส่งมา
    -เพิ่มmethod doPostมารองรับการ requst response

--- HelloServlet/src/main/java/th/ac/ku/kps/eng/cpe/servlet/CustomerServlet.java
+++ HelloServlet/src/main/java/th/ac/ku/kps/eng/cpe/servlet/CustomerServlet.java
@@ -2,7 +2,11 @@ package th.ac.ku.kps.eng.cpe.servlet;
 
 import java.io.ByteArrayOutputStream;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import javax.servlet.ServletException;
@@ -19,6 +23,39 @@ public class CustomerServlet extends HttpServlet {
 	public CustomerServlet() {
 // TODO Auto-generated constructor stub
 	}
+	public static String getBody(HttpServletRequest request) throws IOException {
+
+		String body = null;
+		StringBuilder stringBuilder = new StringBuilder();
+		BufferedReader bufferedReader = null;
+
+		try {
+			InputStream inputStream = request.getInputStream();
+			if (inputStream != null) {
+				bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+				char[] charBuffer = new char[128];
+				int bytesRead = -1;
+				while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+					stringBuilder.append(charBuffer, 0, bytesRead);
+				}
+			} else {
+				stringBuilder.append("");
+			}
+		} catch (IOException ex) {
+			throw ex;
+		} finally {
+			if (bufferedReader != null) {
+				try {
+					bufferedReader.close();
+				} catch (IOException ex) {
+					throw ex;
+				}
+			}
+		}
+
+		body = stringBuilder.toString();
+		return body;
+	}
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter out = resp.getWriter();
@@ -30,4 +67,15 @@ public class CustomerServlet extends HttpServlet {
 		byte[] data = op.toByteArray();
 		out.write(new String(data).toString());
 	}
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		resp.setContentType("application/json");
+		ArrayList<Customer> listOfCustomers = CustomerDAO.getAllCustomers();
+		ByteArrayOutputStream op = new ByteArrayOutputStream();
+		ObjectMapper mapper = new ObjectMapper();
+		String s = getBody(req);
+		out.write("Massage: "+s);
+
+	}
+
 }

197349f6edd44c360b1b3a91699172522c1c4112
Ryker212 <103985410+Ryker212@users.noreply.github.com>
2024-01-12 13:21:00 +0700

    เพิ่มclass testdoPost
    -method getBody มารับค่าClient
    -method doPost มาทำ req resp

---
+++ HelloServlet/src/main/java/th/ac/ku/kps/eng/cpe/servlet/TestdoPost.java
@@ -0,0 +1,69 @@
+package th.ac.ku.kps.eng.cpe.servlet;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import th.ac.ku.kps.eng.cpe.controller.Customer;
+import th.ac.ku.kps.eng.cpe.dao.CustomerDAO;
+
+public class TestdoPost extends HttpServlet{
+	public TestdoPost() {
+		// TODO Auto-generated constructor stub
+			}
+	public static String getBody(HttpServletRequest request) throws IOException {
+
+		String body = null;
+		StringBuilder stringBuilder = new StringBuilder();
+		BufferedReader bufferedReader = null;
+
+		try {
+			InputStream inputStream = request.getInputStream();
+			if (inputStream != null) {
+				bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+				char[] charBuffer = new char[128];
+				int bytesRead = -1;
+				while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+					stringBuilder.append(charBuffer, 0, bytesRead);
+				}
+			} else {
+				stringBuilder.append("");
+			}
+		} catch (IOException ex) {
+			throw ex;
+		} finally {
+			if (bufferedReader != null) {
+				try {
+					bufferedReader.close();
+				} catch (IOException ex) {
+					throw ex;
+				}
+			}
+		}
+
+		body = stringBuilder.toString();
+		return body;
+	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		resp.setContentType("application/json");
+		ArrayList<Customer> listOfCustomers = CustomerDAO.getAllCustomers();
+		ByteArrayOutputStream op = new ByteArrayOutputStream();
+		ObjectMapper mapper = new ObjectMapper();
+		String s = getBody(req);
+		out.write("Massage: "+s);
+
+	}
+}
